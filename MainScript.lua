813123674bd21941c83e87bf303cfbb4ab0605b3
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-01-08 18:15:09 -0500

    add getcustommodassetfunc

--- MainScript.lua
+++ MainScript.lua
@@ -151,7 +151,7 @@ checkpublicvaperepo = function(id)
 end
 checkpublicrepo = function(id)
 	local suc, req = pcall(function() return requestfunc({
-		Url = "https://raw.githubusercontent.com/".. gitUser .."/"..gitRepo .."/main/CustomModules/"..id..".lua",
+		Url = "https://raw.githubusercontent.com/".. shared.gitUser .."/"..shared.gitRepo .."/main/CustomModules/"..id..".lua",
 		Method = "GET"
 	}) end)
 	if not suc then
@@ -183,6 +183,31 @@ local function getcustomassetfunc(path)
 		end)
 		local req = requestfunc({
 			Url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/"..path:gsub("vape/assets", "assets"),
+			Method = "GET"
+		})
+		writefile(path, req.Body)
+	end
+	return getasset(path) 
+end
+
+local function getcustommodassetfunc(path)
+	if not betterisfile(path) then
+		task.spawn(function()
+			local textlabel = Instance.new("TextLabel")
+			textlabel.Size = UDim2.new(1, 0, 0, 36)
+			textlabel.Text = "Downloading "..path
+			textlabel.BackgroundTransparency = 1
+			textlabel.TextStrokeTransparency = 0
+			textlabel.TextSize = 30
+			textlabel.Font = Enum.Font.SourceSans
+			textlabel.TextColor3 = Color3.new(1, 1, 1)
+			textlabel.Position = UDim2.new(0, 0, 0, -36)
+			textlabel.Parent = GuiLibrary["MainGui"]
+			repeat task.wait() until betterisfile(path)
+			textlabel:Remove()
+		end)
+		local req = requestfunc({
+			Url = "https://raw.githubusercontent.com/".. shared.gitUser .."/"..shared.gitRepo .."/main/"..path:gsub("vape/assets", "assets"),
 			Method = "GET"
 		})
 		writefile(path, req.Body)
